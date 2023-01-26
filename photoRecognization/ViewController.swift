742ae534a64c0833be0ecd6469b63b5686314f27
sakiika-lit <saki.cec.419@gmail.com>
2023-01-26 10:57:33 +0900

    Initial Commit

---
+++ photoRecognization/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  photoRecognization
+//
+//  Created by saki shishikura on 2023/01/22.
+//
+
+import UIKit
+import Vision
+import VisionKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var textView: UITextView!
+    var requests = [VNRequest]()
+    var resultingText = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupVision()
+    }
+    
+    func setupVision(){
+        let textRecognitionRequest = VNRecognizeTextRequest { request, _ in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                print("The observations are of an unexpected type.")
+                return
+            }
+            
+            let maximumCandidates = 1
+            for observation in observations {
+                guard let candidate = observation.topCandidates(maximumCandidates).first else { continue }
+                self.resultingText += candidate.string + "\n"
+            }
+        }
+            
+            textRecognitionRequest.recognitionLevel = .accurate
+            self.requests = [textRecognitionRequest]
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+            let documentCameraViewController = VNDocumentCameraViewController(
+                
+                recognizedDataTypes: [.text(languages: ["ja"])]
+            
+            )
+        
+            documentCameraViewController.delegate = self
+            present(documentCameraViewController, animated: true)
+        }
+}
+
+extension ViewController: VNDocumentCameraViewControllerDelegate {
+
+    // DocumentCamera で画像の保存に成功したときに呼ばれる
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        controller.dismiss(animated: true)
+
+        // Dispatch queue to perform Vision requests.
+        let textRecognitionWorkQueue = DispatchQueue(label: "TextRecognitionQueue",
+                                                             qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+        textRecognitionWorkQueue.async {
+            self.resultingText = ""
+            for pageIndex in 0 ..< scan.pageCount {
+                let image = scan.imageOfPage(at: pageIndex)
+                if let cgImage = image.cgImage {
+                    let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+
+                    do {
+                        try requestHandler.perform(self.requests)
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            DispatchQueue.main.async(execute: {
+                // textViewに表示する
+                self.textView.text = self.resultingText
+            })
+        }
+    }
+}
